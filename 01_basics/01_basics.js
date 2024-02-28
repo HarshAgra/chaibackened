82c41bbb206ba705475e0f084261a8345c0df249
HarshAgra <160864225+HarshAgra@users.noreply.github.com>
2024-02-28 07:00:32 +0000

    learning JavaScript

---
+++ 01_basics/01_basics.js
@@ -0,0 +1,6 @@
+const accountId= 144553;
+let accountEmail= "harsh@google.com";
+var accountPassword= "xyz";
+accountCity= "Prayagraj"
+
+console.table([accountEmail, accountId, accountPassword, accountCity ]);
